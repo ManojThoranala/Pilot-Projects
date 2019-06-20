914a3ba06418b65b0b873dfdfa8857aaa6726c28
Manoj Thoranala Manjunatha <manojthoranala@gmail.com>
2019-06-20 23:24:26 +0530

    Update StringReverse.c

--- StringReverse.c
+++ StringReverse.c
@@ -1,3 +1,8 @@
+/***************************************************************
+*
+**********************************************************/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -25,4 +30,48 @@ void mian()
 	string_reverse(string);
 	printf("Here is the reversed string\n");
 	printf("%s", string_reversed);
+}
+
+
+/***********************************************************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int string_revese (char * str)
+{  
+    int str_len = 0;
+    char * ch = str;
+	char temp;
+    while(* ch++ != '\n') str_len++;
+	ch--;
+    for (int i = 0; i <= (str_len - 1) / 2; i++, ch--){
+		temp = * ch;
+		* ch = str[i];
+		str[i] = temp;
+    }
+	return 0;
+}
+
+int string_reverse_xor (char * str)
+{
+	int str_len = 0;
+    char * ch = str;
+    while(* ch++ != '\n') str_len++;
+	for (int i = 0; i <= (str_len - 1) / 2; i++){
+		str[i] = str[i] ^ str[str_len-1-i];
+		str[i] = str[i] ^ str[str_len-1-i];
+		str[str_len-1-i] = str[i] ^ str[str_len-1-i];
+	}
+}
+
+void main()
+{
+	char * string;
+	string = malloc(256 * sizeof(char));
+	printf("Enter the string to make reverse:\n");
+	fgets(string, 256, stdin);
+	string_reverse(string);
+	printf("Here is the reversed string\n");
+	printf("%s", string);
 }
