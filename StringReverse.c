e68cc6e376cf2347717ae1ec42d30619a42d635b
Manoj Thoranala Manjunatha <manojthoranala@gmail.com>
2019-06-20 23:05:13 +0530

    Update StringReverse.c

--- StringReverse.c
+++ StringReverse.c
@@ -6,9 +6,11 @@ int string_revese (char * str, char * reversed_str)
     int str_len = 0;
     char * ch = str;
     while(* ch++ != '\n') str_len++;
-    for (int i = str_len - 1; i < = 0 && str[i] != '\n'; i--, reversed_str++){
+    for (int i = str_len - 1; i < = 0; i--, reversed_str++){
 		* reversed_str = str[i];
     }
+    reversed_str[1] = '\n';
+    reversed_str[2] = '\0';
 	return 0;
 }
 
